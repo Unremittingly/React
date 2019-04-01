d10d26d88fac6cb328b7e97096ce9f1acdad5d55
zhangqiang <13608164790zq93>
2019-04-01 13:34:42 +0800

    提取公共 post  get 请求

--- src/helpers/dataManage.jsx
+++ src/helpers/dataManage.jsx
@@ -3,31 +3,28 @@ import {loginIn} from "../redux/userInfo/action";
 import qs from 'qs';
 // import {LOGIN_IN} from "../redux/userInfo/constant";
 
+/********
+ * 获取文章列表
+ * @param url
+ * @param params
+ * @param callBack
+ */
 export const getArticles = (url, params, callBack) => {
-    axios.get(url, {params: params}).then(function (res) {
-        console.log('res', res);
-        if (res.status === 200) {
-            // if(dispatch){
-            //     dispatch(getList({
-            //         data: res.data,
-            //         state: false
-            //     }));
-            // }
 
-            if (callBack) {
-                callBack(res.data);
-            }
-
-        } else if (res.status === 404) {
-            console.log('not found');
+    getUrl(url,params).then((result)=>{
+        if(callBack){
+            callBack(result)
         }
-    }, function (error) {
-
-    }).catch(function (error) {
-        console.log('error', error);
     });
 };
 
+
+
+/**********
+ * 保存文章
+ * @param url
+ * @param params
+ */
 export const saveArticle = (url, params,) => {
     axios.post(url, qs.stringify(params)).then(function (res) {
         if (res.status === 200) {
@@ -41,12 +38,18 @@ export const saveArticle = (url, params,) => {
     })
 };
 
+/*******
+ * 登录验证
+ * @param url
+ * @param param
+ * @param dispatch
+ * @returns {Promise<any | never>}
+ */
+
 export const login = function (url, param, dispatch) {
 
 
-
-
-    return new Promise((resolve, reject) =>  {
+    return new Promise((resolve, reject) => {
         axios.post(url, qs.stringify(param), {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
@@ -59,7 +62,6 @@ export const login = function (url, param, dispatch) {
                     console.log('请求');
                     dispatch(loginIn(param));
                     resolve(res.data);
-
                 } else {
                     console.log('账号或密码错误');
                     reject();
@@ -70,57 +72,93 @@ export const login = function (url, param, dispatch) {
         }).catch(function (e) {
             console.log('未知错误', e);
         });
-    }).catch((e)=>{
-        console.log('异常',e);
+    }).catch((e) => {
+        console.log('异常', e);
     });
 
-
-
 };
 
-export const getToken = () => {
-
-};
 
 export const getTags = () => {
 
 };
 
-export const postUrl = function* (url, params) {
-    let result = {};
-    yield axios.post(url, qs.stringify(params), {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).then(function (res) {
-        if (res.status === 200) {
-            if (res.data.isSuccess) {
-                console.log('操作成功');
-                result = res;
-            } else {
-                console.log('操作失败');
-            }
-
-        }
-    });
-    yield result;
-};
-
+/*******
+ * 通过id  获取文章内容
+ * @param url
+ * @param params
+ * @param callback
+ * @returns {*}
+ */
 
 export const getArticleForId = (url, params, callback) => {
-    axios.post(url, qs.stringify(params)).then(function (res) {
-        if (res.status === 200) {
-            if (res.data.isSuccess) {
-                console.log('查询成功', res.data);
-                if (callback) {
-                    callback(res.data.data)
-                }
-            } else {
-                console.log('插入失败');
-            }
 
+    return postUrl(url,params).then((res)=>{
+        if(callback){
+            callback(res.data)
         }
-    })
+    });
+
 };
+
+
+/********
+ * 公共post请求封装
+ * @param url
+ * @param params
+ * @param hint
+ * @returns {Promise<any | never>}
+ */
+
+export const postUrl = (url, params,hint) => {
+
+    return new Promise((resolve, reject) => {
+        axios.post(url, qs.stringify(params), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(function (res) {
+            if (res.status === 200) {
+                if (res.data.isSuccess) {
+                    console.log('操作成功');
+                    resolve(res.data)
+                } else {
+                    console.log('操作失败',hint);
+                }
+
+            }
+        });
+    }).catch((e)=>{
+        console.log('未知错误',e);
+    });
+
+};
+
+
+/*******
+ * 公共get请求封装
+ * @param url
+ * @param params
+ * @returns {Promise<any | never>}
+ */
+
+export const getUrl = (url, params)=>{
+    return new Promise((resolve, reject) => {
+        axios.get(url, {params: params}).then(function (res) {
+            console.log('res', res);
+            if (res.status === 200) {
+                resolve(res.data);
+            } else if (res.status === 404) {
+                console.log('not found');
+                reject();
+            }
+        });
+    }).catch((e)=>{
+        console.log('未知错误',e);
+    })
+
+};
+
+
 
 
