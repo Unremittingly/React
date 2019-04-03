768244b3af552db47174135a8ed203cdaf8a1ed3
zhangqiang <13608164790zq93>
2019-04-03 16:43:03 +0800

    test

--- src/common/sidebar/ClassifyItem.jsx
+++ src/common/sidebar/ClassifyItem.jsx
@@ -9,38 +9,39 @@ import './classfiyItem..scss';
 class ClassifyItem extends Component {
 
     state = {
-        data:this.props.list.data
+        data: this.props.list.data,
     };
 
     getNewListDom(data) {
         let doms = [];
         let length = data.length;
         for (let i = length - 1; i >= 0; i--) {
-            let item =data[i];
-            if(item.url){
+            let item = data[i];
+            if (item.url) {
                 doms.push(<a key={i} className="sidebar-item" href={item.url} target="_blank">{item.title}</a>);
-            }else{
-                doms.push(<Link key={i} className="sidebar-item" target="_blank" rel="noopener noreferrer" to={"/article/detail/" + item.id} >{item.title}</Link>);
+            } else {
+                doms.push(<Link key={i} className="sidebar-item" target="_blank" rel="noopener noreferrer" to={"/article/detail/" + item.id}>{item.title}</Link>);
             }
 
             if (i < length - 3) {
                 break;
             }
         }
-        return doms;
+        return doms.length>0?doms:<div className="no-content">暂无内容</div>;
     }
 
     render() {
         return (
             <div className="classify-item">
                 {/*最新*/}
-                <div className="item-tit">{this.props.list.title}</div>
+                <div className="item-tit" >{this.props.list.title}</div>
                 {this.getNewListDom(this.props.list.data)}
                 {/*所有*/}
             </div>
         );
     }
 }
+
 ClassifyItem.propTypes = {
     list: PropType.object
 };
