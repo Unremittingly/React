fe458864f620b19b4c89eb1baf04b85051c7d5ca
object <>
2020-01-17 17:32:14 +0800

    music

--- src/view/music/list/index.jsx
+++ src/view/music/list/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './index.scss';
 import { Spin, Icon } from 'antd';
-
+import { getUrl } from '../../../helpers/dataManage';
 
 const MusicList = (props)=>{
 
     const {data} = props;
     const {songs,isLoading} = data;
+
+    const getSongUrl= ()=>{
+      const url = 'http://api.mtnhao.com/song/url';
+        getUrl(url,{
+            id:33894312
+        }).then((data)=>{
+            console.log('data');
+        })
+    };
 
     if(isLoading){
         return <div className="music-list"><Spin /></div>;
@@ -22,7 +31,7 @@ const MusicList = (props)=>{
                             return (<span className="artist-item">{artist.name}</span>)
                         })}
                     </div>
-                    <div className="optGroup"><Icon type="download"/> </div>
+                    <div className="optGroup"> <a href="http://m8.music.126.net/20200117175416/a33d2b2c18f50df71409fcec2d1ca164/ymusic/0fd6/4f65/43ed/a8772889f38dfcb91c04da915b301617.mp3" download="t.mp3">down</a><Icon type="download"/> </div>
 
                 </div>
             }) :<div>
