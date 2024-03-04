c4af115bfa1578041f554cb7518a4363c90bb77d
张强 <>
2024-03-04 15:13:13 +0800

    feat:音频跳动动画

--- src/view/example/audioJitter/index.js
+++ src/view/example/audioJitter/index.js
@@ -1,55 +1,64 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
 import {Button} from "antd";
 const remindSrc = require('@/view/example/audioJitter/宁夏-梁静茹.mp3')
 
 let isStop = false;
-const onDraw = (oAudio)=>{
-  // 创建音频上下文对象
-  const oCtx = new AudioContext();
-  // console.log(oCtx);
-  // 创建媒体源,除了audio本身可以获取，也可以通过oCtx对象提供的api进行媒体源操作
-  const audioSrc = oCtx.createMediaElementSource(oAudio);
-  // 创建分析机
-  const analyser = oCtx.createAnalyser();
-  // 媒体源与分析机连接
-  audioSrc.connect(analyser);
-  // 输出的目标：将分析机分析出来的处理结果与目标点（耳机/扬声器）连接
-  analyser.connect(oCtx.destination);
+// 创建音频上下文对象
+const oCtx = new AudioContext();
+let audioSrc,analyser,ctx,oW,oH,color1,color2;
 
-  // 效果（实现的具体方法）
-  // 绘制音频图的条数(fftSize)
-  /*
-    根据分析音频的数据去获取音频频次界定音频图的高度
-    放在与音频频次等长的8位无符号字节数组
-    Uint8Array:初始化默认值为1024
-  */
-  // 利用cancas渐变进行音频绘制
-  // eslint-disable-next-line no-undef
-  var ctx = canvas.getContext('2d');
-  // eslint-disable-next-line no-undef
-  canvas.width = window.innerWidth;
-  // eslint-disable-next-line no-undef
-  canvas.height = window.innerHeight/2;
-  // eslint-disable-next-line no-undef
-  const oW = canvas.width;
-  // eslint-disable-next-line no-undef
-  const oH = canvas.height;
-  const color1 = ctx.createLinearGradient(oW / 2, oH / 2 - 30, oW / 2, oH / 2 - 100);
-  const color2 = ctx.createLinearGradient(oW / 2, oH / 2 + 30, oW / 2, oH / 2 + 100);
-  color1.addColorStop(0, '#000');
-  color1.addColorStop(.5, '#069');
-  color1.addColorStop(1, '#3b6abf');
-  color2.addColorStop(0, '#000');
-  color2.addColorStop(.5, '#069');
-  color2.addColorStop(1, '#265f04');
-  // 音频图的条数
-  var count = 150;
-  // 缓冲区:进行数据的缓冲处理，转换成二进制数据
-  var voiceHeight = new Uint8Array(analyser.frequencyBinCount);
+const onDraw = (oAudio)=>{
+
+  if(!audioSrc){
+    // console.log(oCtx);
+    // 创建媒体源,除了audio本身可以获取，也可以通过oCtx对象提供的api进行媒体源操作
+    audioSrc = oCtx.createMediaElementSource(oAudio);
+    // 创建分析机
+    analyser = oCtx.createAnalyser();
+    // 媒体源与分析机连接
+    audioSrc.connect(analyser);
+    // 输出的目标：将分析机分析出来的处理结果与目标点（耳机/扬声器）连接
+    analyser.connect(oCtx.destination);
+
+    // 效果（实现的具体方法）
+    // 绘制音频图的条数(fftSize)
+    /*
+      根据分析音频的数据去获取音频频次界定音频图的高度
+      放在与音频频次等长的8位无符号字节数组
+      Uint8Array:初始化默认值为1024
+    */
+    // 利用cancas渐变进行音频绘制
+    // eslint-disable-next-line no-undef
+    ctx = canvas.getContext('2d');
+    // eslint-disable-next-line no-undef
+    canvas.width = window.innerWidth;
+    // eslint-disable-next-line no-undef
+    canvas.height = window.innerHeight/2;
+    // eslint-disable-next-line no-undef
+    oW = canvas.width;
+    // eslint-disable-next-line no-undef
+    oH = canvas.height;
+
+     color1 = ctx.createLinearGradient(oW / 2, oH / 2 - 30, oW / 2, oH / 2 - 100);
+     color2 = ctx.createLinearGradient(oW / 2, oH / 2 + 30, oW / 2, oH / 2 + 100);
+    color1.addColorStop(0, '#000');
+    color1.addColorStop(.5, '#069');
+    color1.addColorStop(1, '#3b6abf');
+    color2.addColorStop(0, '#000');
+    color2.addColorStop(.5, '#069');
+    color2.addColorStop(1, '#265f04');
+  }
+
+
+
   // console.log(voiceHeight);
 
   function draw() {
+    // 音频图的条数
+    var count = 150;
+    // 缓冲区:进行数据的缓冲处理，转换成二进制数据
+    var voiceHeight = new Uint8Array(analyser.frequencyBinCount);
     // 将当前的频率数据复制到传入的无符号字节数组中，做到实时连接
     analyser.getByteFrequencyData(voiceHeight);
     // console.log(voiceHeight);
@@ -61,11 +70,11 @@ const onDraw = (oAudio)=>{
     for (var i = 0; i < count; i++) {
       var audioHeight = voiceHeight[step * i];
       ctx.fillStyle = color1;  // 绘制向上的线条
-      ctx.fillRect(oW / 2 + (i * 10), oH / 2, 3, -audioHeight);
-      ctx.fillRect(oW / 2 - (i * 10), oH / 2, 3, -audioHeight);
+      ctx.fillRect(oW / 2 + (i * 10), oH / 2, 3, -audioHeight/2);
+      ctx.fillRect(oW / 2 - (i * 10), oH / 2, 3, -audioHeight/2);
       ctx.fillStyle = color2;  // 绘制向下的线条
-      ctx.fillRect(oW / 2 + (i * 10), oH / 2, 3, audioHeight);
-      ctx.fillRect(oW / 2 - (i * 10), oH / 2, 3, audioHeight);
+      ctx.fillRect(oW / 2 + (i * 10), oH / 2, 3, audioHeight/2);
+      ctx.fillRect(oW / 2 - (i * 10), oH / 2, 3, audioHeight/2);
     }
     if(!isStop){
       window.requestAnimationFrame(draw);
@@ -84,18 +93,23 @@ const onDraw = (oAudio)=>{
 }
 
 const AudioJitter = ()=>{
+  const [oAudio, setAudio] =useState();
+  useEffect(()=>{
+    const oA = document.getElementById('myAudio');
+    setAudio(oA);
+  },[])
   const onStart = ()=>{
-    const oAudio = document.getElementById('myAudio');
-    if (oAudio.paused) {
-      oAudio.play();
-      onDraw(oAudio);
-      isStop=false;
-    } else {
-      oAudio.pause();
-      setTimeout(()=>{
+    if(oAudio){
+      if (oAudio.paused) {
+        isStop=false;
+        oAudio.play();
+        onDraw(oAudio);
+      } else {
         isStop = true;
-      },1000)
+        oAudio.pause();
+      }
     }
+
   }
   return <div>
     <Button onClick={onStart}>Start</Button>
