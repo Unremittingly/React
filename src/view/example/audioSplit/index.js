73e7725bd8c995a5b668e3418dd65e142a010db7
张强 <>
2025-05-09 15:01:40 +0800

    一些实用脚本

---
+++ src/view/example/audioSplit/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * 音频分离人声和背景声音
+ * @return {JSX.Element}
+ * @constructor
+ */
+const AudioSplit = ()=>{
+
+  const handleSplitAudio = ()=>{
+
+  }
+  return <>
+
+  </>
+}
---
+++ src/view/example/tone/index.jsx
@@ -0,0 +1,19 @@
+import {useEffect} from "react";
+
+
+const ToneE = ()=>{
+
+  // useEffect(()=>{
+  //   const synth = new Tone.Synth().toDestination();
+  //   const now = Tone.now();
+  //   // trigger the attack immediately
+  //   synth.triggerAttack("C4", now);
+  //   // wait one second before triggering the release
+  //   synth.triggerRelease(now + 1);
+  // },[])
+
+  return <>
+
+  </>
+}
+export default ToneE;
